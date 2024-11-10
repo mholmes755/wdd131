7b685792c6b742b855949619f01b846f57bc28a6
mholmes755 <hol23026@byui.edu>
2024-11-09 20:18:29 -0700

    W01 Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,18 @@
+
+// use the date object
+const currentDate = new Date();
+
+
+// weekday: 'long', month: 'short', day: 'numeric',
+const options = {year: 'numeric'};
+const currentYear = currentDate.toLocaleDateString('en-US', options);
+const copyrightYear = '&copy' + currentYear;
+
+document.getElementById('copyrightYear').textContent = currentYear;
+
+
+
+// Last Modified
+const lastModif = document.lastModified;
+const lastModifText = 'Last Modified: ' + lastModif;
+document.getElementById('lastModified').textContent = lastModifText;
